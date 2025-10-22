339540347898e283f2ba5c191ee1d46658bba324
ashrithat <138019339+ashrithat@users.noreply.github.com>
2025-10-22 07:49:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,4 @@ resource "aws_instance" "servers" {
         }
 }
 
-resource "aws_instance" "imported_server" {
-        ami = "ami-0199d4b5b8b4fde0e"
-        instance_type = "t2.micro"
-        subnet_id = "subnet-032ba554994e26a59"
-        vpc_security_group_ids = [ "sg-052121cc0522de14e"]
-        associate_public_ip_address = true
 
-        tags = {
-          Name = "addon-server"
-        }
-}
