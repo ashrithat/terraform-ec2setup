a2a3cb2f05039a24637f59b2663e5c6c06d8830a
ashrithat <138019339+ashrithat@users.noreply.github.com>
2025-10-24 06:29:42 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-s3-bcktremote" 
-    key            = "dev/terraform.tfstate"          
-    region         = "us-east-2"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
